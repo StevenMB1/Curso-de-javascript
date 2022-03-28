390fcc4a478b6b410992cbde03473620a2dcb06e
Steven Medina <74547441+StevenMB1@users.noreply.github.com>
2022-03-28 11:34:17 -0400

    actualizacion

---
+++ curso-de-javascript-parte-2/js/dom.js
@@ -0,0 +1,40 @@
+//? document
+
+// console.log('asasdd')
+
+// console.log(document)
+// console.log(document.head)
+// console.log(document.title)
+
+
+//? getElementById()
+
+// console.log(document.getElementById('tituloWeb'))
+// console.log(document.getElementById('tituloWeb').textContent)
+
+//? script, DOMContent, Loaded y defer
+
+// document.addEventListener('DOMContentLoader', () => {
+//     console.log(document.getElementById('tituloWeb'))
+//     console.log(document.getElementById('tituloWeb').textContent)
+// })
+
+
+// console.log(document.getElementById('tituloWeb'))
+// console.log(document.getElementById('tituloWeb').textContent)
+
+//? querySelector() - Es un meotodo.
+
+// console.log(document.querySelector('#tituloWeb'))
+// console.log(document.querySelector('.text-primary'))
+// console.log(document.querySelector('h1'))
+
+// console.log(document.querySelector('.text-danger')) // Selecciona el primer elemento que encuentra
+// console.log(document.querySelectorAll('.text-danger')) // Selecciona todos los elementos
+// console.log(document.querySelectorAll('.container .text-danger')) // Selecciona unicamente las clases que estan dentro del text-danger.
+
+
+//? querySelector vs getElementById
+
+
+
