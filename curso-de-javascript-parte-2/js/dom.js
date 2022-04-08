6feba88356d31063eddce7746c20be1e50d82f53
Steven Medina <74547441+StevenMB1@users.noreply.github.com>
2022-04-08 15:56:25 -0400

    Actualizacion

--- curso-de-javascript-parte-2/js/dom.js
+++ curso-de-javascript-parte-2/js/dom.js
@@ -162,3 +162,26 @@
 
 
 //? Template
+
+const lista = document.querySelector('#lista')
+const fragment = document.createDocumentFragment()
+const liTemplate = document.querySelector('#liTemplate')
+
+const arrayPaises = ['Peru', 'Mexico', 'Colombia']
+
+arrayPaises.forEach( pais => {
+
+    const clone = liTemplate.content.cloneNode(true)
+    clone.querySelector('span').textContent = pais
+
+    fragment.appendChild(clone)
+
+})
+
+lista.appendChild(fragment)
+
+// const clone = liTemplate.content.cloneNode(true)
+
+// clone.querySelector('.text-primary').textContent = 'Agregue a travez de un template'
+
+// lista.appendChild(clone)
